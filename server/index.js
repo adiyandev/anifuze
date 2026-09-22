bb76a8734c2468f9526a866836630b54c55c6ed5
ADIYAN <adiyanhehe@gmail.com>
2026-09-22 19:34:37 +0700

    feat: mount streaming provider routes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import {installerRouter} from './routes/installer.js';
 import {authRouter} from './routes/auth.js';
 import {anilistRouter} from './routes/anilist.js';
 import {episodesRouter} from './routes/episodes.js';
+import {providersRouter} from './routes/providers.js';
 import {isInstallerLocked} from './installer/index.js';
 
 const app=express();
@@ -17,6 +18,7 @@ app.use('/api/installer',installerRouter);
 app.use('/api/auth',authRouter);
 app.use('/api',anilistRouter);
 app.use('/api',episodesRouter);
+app.use('/api',providersRouter);
 app.get('/api/system/install',(_req,res)=>res.json({installationId:config.installationId,domain:config.domain,nodeEnv:config.nodeEnv}));
 
 const start=async()=>{if(await isInstallerLocked())await runMigrations();app.listen(config.port,()=>console.log('AniFuze server listening on :' + config.port));};
