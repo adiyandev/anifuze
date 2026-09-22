1762c4f9de0803fb2b5117faf77f967a568cc763
ADIYAN <adiyanhehe@gmail.com>
2026-09-22 19:28:14 +0700

    feat: mount episode management routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import {runMigrations} from './migrate.js';
 import {installerRouter} from './routes/installer.js';
 import {authRouter} from './routes/auth.js';
 import {anilistRouter} from './routes/anilist.js';
+import {episodesRouter} from './routes/episodes.js';
 import {isInstallerLocked} from './installer/index.js';
 
 const app=express();
@@ -15,6 +16,7 @@ app.get('/api/health',async(_req,res)=>{try{res.json({ok:true,service:'anifuze',
 app.use('/api/installer',installerRouter);
 app.use('/api/auth',authRouter);
 app.use('/api',anilistRouter);
+app.use('/api',episodesRouter);
 app.get('/api/system/install',(_req,res)=>res.json({installationId:config.installationId,domain:config.domain,nodeEnv:config.nodeEnv}));
 
 const start=async()=>{if(await isInstallerLocked())await runMigrations();app.listen(config.port,()=>console.log('AniFuze server listening on :' + config.port));};
