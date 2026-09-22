06c1f24bf9b650c3ee5eb2c0f253f286eac916d0
ADIYAN <adiyanhehe@gmail.com>
2026-09-22 20:20:08 +0700

    test: cover complete predefined page set

--- server/services/pages.test.js
+++ server/services/pages.test.js
@@ -3,5 +3,5 @@ import assert from 'node:assert/strict';
 import {DEFAULT_PAGE_SLUGS} from './pages.js';
 
 test('default page contract contains only predefined template pages',()=>{
- assert.deepEqual([...DEFAULT_PAGE_SLUGS].sort(),['anime','browse','genres','home','latest','login','register','schedule','search','trending','watch']);
+ assert.deepEqual([...DEFAULT_PAGE_SLUGS].sort(),['anime','browse','continue-watching','favorites','genres','history','home','latest','login','profile','register','schedule','search','trending','watch','watchlist']);
 });
