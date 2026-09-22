f6710b422c74d21993c1ea7762d0c30abb6f8d3b
ADIYAN <adiyanhehe@gmail.com>
2026-09-22 19:57:34 +0700

    feat: mount template engine routes

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import {providerConsoleRouter} from './routes/providerConsole.js';
 import {providerHealthRouter} from './routes/providerHealth.js';
 import {publicCatalogRouter} from './routes/publicCatalog.js';
 import {homepageRouter} from './routes/homepage.js';
+import {templateRouter} from './routes/templates.js';
 import {isInstallerLocked} from './installer/index.js';
 
 const app=express();
@@ -27,6 +28,7 @@ app.use('/api',providerConsoleRouter);
 app.use('/api',providerHealthRouter);
 app.use('/api',publicCatalogRouter);
 app.use('/api',homepageRouter);
+app.use('/api',templateRouter);
 app.get('/api/system/install',(_req,res)=>res.json({installationId:config.installationId,domain:config.domain,nodeEnv:config.nodeEnv}));
 
 const start=async()=>{if(await isInstallerLocked())await runMigrations();app.listen(config.port,()=>console.log('AniFuze server listening on :' + config.port));};
