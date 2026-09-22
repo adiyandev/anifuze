00bdc17e7cd655359593ae4ce5fbc0cb64e9c30c
ADIYAN <adiyanhehe@gmail.com>
2026-09-22 21:57:36 +0700

    feat: mount notification API

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ import {pagesRouter} from './routes/pages.js';
 import {seoRouter} from './routes/seo.js';
 import {usersRouter} from './routes/users.js';
 import {communityRouter} from './routes/community.js';
+import {notificationsRouter} from './routes/notifications.js';
 import {isInstallerLocked} from './installer/index.js';
 
 const app=express();
@@ -48,6 +49,7 @@ app.use('/api',pagesRouter);
 app.use('/api',seoRouter);
 app.use('/api',usersRouter);
 app.use('/api',communityRouter);
+app.use('/api',notificationsRouter);
 app.get('/api/system/install',(_req,res)=>res.json({installationId:config.installationId,domain:config.domain,nodeEnv:config.nodeEnv}));
 
 const start=async()=>{if(await isInstallerLocked())await runMigrations();app.listen(config.port,()=>console.log('AniFuze server listening on :' + config.port));};
