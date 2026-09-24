617cb8ddec455c66e3c8a9b764e30578ad77acb1
ADIYAN <adiyanhehe@gmail.com>
2026-09-24 20:22:50 +0700

    refactor(layout): update installer system boundaries

--- installer/server/license.js
+++ installer/server/license.js
@@ -1,5 +1,5 @@
 import crypto from 'node:crypto';
-import {config} from '../config.js';
+import {config} from '../../system/server/config.js';
 
 export function getInstallationIdentity(){return {installationId:config.installationId,domain:config.domain,licenseKey:config.licenseKey};}
 export async function verifyLicenseKey(licenseKey){
